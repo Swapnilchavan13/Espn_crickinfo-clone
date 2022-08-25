1a4d2657164e6286c12b3d2e97862115af9e8eed
anmol7895 <anmolpandit1023@gmail.com>
2022-08-25 18:43:18 +0530

    day-3 tasks completed

--- js/series.js
+++ js/series.js
@@ -1,23 +1,13 @@
-let top_ads = ["https://tpc.googlesyndication.com/simgad/4599272747741584314?",
-    "https://tpc.googlesyndication.com/simgad/4806003440218956751?"]
+let top_ads = ["https://tpc.googlesyndication.com/simgad/11321537340682502286?",
+    "https://tpc.googlesyndication.com/simgad/4271044209677937619?"]
 
-let right_ads = ["https://tpc.googlesyndication.com/simgad/12365718001132389279?",
-    "https://tpc.googlesyndication.com/simgad/7411421147266363877?",
-    "https://tpc.googlesyndication.com/simgad/12598192234913773850?",
-    "https://tpc.googlesyndication.com/simgad/15841686395453576998?",
-    "https://tpc.googlesyndication.com/simgad/15582684688719062778?",
-    "https://tpc.googlesyndication.com/simgad/12239562037567381132?",
-    "https://tpc.googlesyndication.com/simgad/6181019838457675129?",
-    "https://tpc.googlesyndication.com/simgad/3541276014299803378?",
-    "https://tpc.googlesyndication.com/simgad/16995279651211156069?",
-    "https://tpc.googlesyndication.com/simgad/4461905471653839514?"]
+
 
 window.addEventListener('load', () => {
     document.getElementById('series').innerHTML = currentCricket();
 
     document.getElementById('topAd').setAttribute('src', `${top_ads[Math.floor(Math.random() * 2)]}`)
-    document.getElementById('rightAd1').setAttribute('src', `${right_ads[Math.floor(Math.random() * right_ads.length)]}`)
-    document.getElementById('rightAd2').setAttribute('src', `${right_ads[Math.floor(Math.random() * right_ads.length)]}`)
+   
 })
 
 let series_div = document.getElementById('series')
@@ -25,3 +15,170 @@ let series_div = document.getElementById('series')
 function asd() {
     console.log('hello')
 }
+
+function series() {
+    let x = event.target
+
+    console.log(x)
+
+    let ser = document.getElementsByClassName('series');
+
+    for (let i = 0; i < ser.length; i++) {
+        console.log(ser[i].setAttribute('style', 'color : #000'))
+    }
+
+    x.setAttribute('style', 'color : #439ec9')
+
+    if (x.id == 1) {
+
+        series_div.innerHTML = '';
+
+        series_div.innerHTML = currentCricket();
+
+    }
+    else if (x.id == 2) {
+
+        series_div.innerHTML = '';
+
+        series_div.innerHTML = futureSeriesTornaments();
+
+        document.getElementById('ser-right').setAttribute('style', 'border-left : none')
+
+    }
+    else {
+
+        series_div.innerHTML = '';
+
+        series_div.innerHTML = recentlyConcluded();
+
+    }
+}
+
+
+
+function currentCricket() {
+    let current_cricket = `<div id="ser-left">
+    <h2>International Tours</h2>
+    <ul>
+
+        <li><a href="#">India vs Zimbabwe </a></li>
+        <li><a href="#">England vs South Africa</a></li>
+        <li><a href="#">Zimbabwe v Pakistan</a></li>
+        <li><a href="#">Nepal Tri-Nation T20I</a></li>
+        <li><a href="#">NZ-A in India</a></li>
+        
+    </ul>
+
+    <h2>International Tournaments</h2>
+    <ul>
+
+        <li><a href="#">Asia Cup Qualifier</a></li>
+        <li><a href="#">Asia Cup 2022</a></li>
+        <li><a href="#">CWC Challenge Group A</a></li>
+        <li><a href="#">CWC Challenge  Group B</a></li>
+        
+    </ul>
+
+  </div>
+
+  <div id="ser-right">
+
+  <h2>ICC Tournaments</h2>
+    <ul>
+
+        <li><a href="#">ICC Women's Championship</a></li>
+        <li><a href="#">Men's Cricket World Cup League 2</a></li>
+        <li><a href="#">CWC Challenge League Group A</a></li>
+        <li><a href="#">CWC Challenge League Group B</a></li>
+        <li><a href="#">ICC CWC Super League</a></li>
+        <li><a href="#">ICC World Test Championship</a></li>
+        
+    </ul>
+
+    <h2>T20/T10 Tournaments</h2>
+    <ul>
+
+        <li><a href="#">Asia Cup 2022</a></li>     
+        <li><a href="#">SLC T20</a></li>
+        <li><a href="#">Maharaja T20</a></li>
+        <li><a href="#">T20 QUAD (in UAE)</a></li>
+        <li><a href="#">Namibia T20</a></li>
+        
+    </ul>
+
+    <h2>Associate Cricket</h2>
+    <ul>
+
+        <li><a href="#">Kenya v Nepal</a></li>                      
+        <li><a href="#">Bahrain v Kuwait</a></li>                      
+        <li><a href="#">ITA-W in Austria</a></li>                   
+        <li><a href="#">Continental Cup</a></li>
+        
+    </ul>
+
+    <h2>Australia Domestic</h2>
+    <ul>
+
+    <li><a href="#">Sheffield Shield</a></li> 
+        <li><a href="#">Big Bash League</a></li>  
+        <li><a href="#">Women's Big Bash League</a></li> 
+        <li><a href="#">Marsh Cup</a></li>   
+        <li><a href="#">WNCL</a></li>   
+        
+        
+    </ul>
+
+    <h2>England Domestic</h2>
+    <ul>
+
+        <li><a href="#">County Div1</a></li>   
+        <li><a href="#">County Div2</a></li>   
+        <li><a href="#">100 (Men's)</a></li>   
+        <li><a href="#">100 (Women's)</a></li>  
+        <li><a href="#">RL One-Day Cup</a></li>   
+        <li><a href="#">RHFT Trophy</a></li>  
+        <li><a href="#">Vitality Blast</a></li>
+        
+    </ul>
+
+    <h2>India Domestic</h2>
+    <ul>
+
+        <li><a href="#">Women's Senior ODI</a></li>
+        <li><a href="#">Vijay Hazare Trophy</a></li>
+        <li><a href="#">SMA Trophy</a></li>
+        
+    </ul>
+
+    
+
+    <h2>New Zealand Domestic</h2>
+    <ul>
+
+        <li><a href="#">Plunket Shield</a></li>
+        <li><a href="#">Ford Trophy</a></li>
+        <li><a href="#">Women's 1-Day</a></li>
+        
+    </ul>
+
+    <h2>West Indies Domestic</h2>
+    <ul>
+
+        <li><a href="#">6IXTY (W)</a></li>
+        <li><a href="#">6IXTY (M)</a></li>
+        <li><a href="#">CPL 2022</a></li>
+        <li><a href="#">WCPL 2022</a></li>
+        
+    </ul>
+
+   
+
+  </div>`
+
+    return current_cricket;
+}
+
+// future and recently
+
+
+
